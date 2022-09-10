ec79b1cf4f20ec50583cab153c9f8b8139384ca5
John Bassey <jsbazman@gmail.com>
2022-09-10 14:35:42 +0100

    Updated Serializer.py

---
+++ countries/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from countries.models import Countries
+
+class CountriesSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Countries
+        fields = ('id', 'name','capital')
